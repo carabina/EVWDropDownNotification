368f1a716e4fd4952baf4014da84952cf9a224df
Eric Weil <ericweil@erics-macbook-pro.local>
2016-06-27 16:07:23 +0200

    Final 0.1.0

---
+++ EVWDropDownNotification/Classes/EVWDropDownNotification.swift
@@ -0,0 +1,361 @@
+//
+//  EVWDropDownNotification.swift
+//
+//
+//  Created by CRE8IT GmbH on 26/06/16.
+//  Copyright © 2016 CRE8IT GmbH. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public protocol EVWDropDownNotificationDelegate {
+    func dropdownNotificationTopButtonTapped()
+    func dropdownNotificationBottomButtonTapped()
+    func dropdownNotificationDismissedWithTap()
+}
+
+public enum ButtonPosition {
+    case Bottom
+    case Top
+}
+
+public class EVWDropDownNotification: NSObject {
+    
+    private var titleLabel:UILabel?
+    private var subTitileLabel:UILabel?
+    private var imageView:UIImageView?
+    private var topButton:UIButton?
+    private var bottomButton:UIButton?
+    
+    private var screenSize:CGSize?
+    private var animator:UIDynamicAnimator?
+    private var gravityAnimation:Bool?
+    private var notificationView:UIView?
+    private var isBeingShown:Bool = false
+    
+    public var delegate:EVWDropDownNotificationDelegate?
+    
+    
+    
+    /*
+     * Padding between elements
+     */
+    public var paddingBetweenElements:CGFloat = 10
+    
+    /*
+     * Background Color If set, it overwrites the blur effect
+     */
+    public var backgroundColor:UIColor?
+    
+    /*
+     * Title Attributes
+     */
+    public var titleLabelFontSize:CGFloat = 19
+    public var titleLabelColor:UIColor = UIColor.blackColor()
+    public var titleText:String?
+    
+    /*
+     * Subtitle Attributes
+     */
+    public var subTitleLabelFontSize:CGFloat = 14
+    public var subTitleLabelColor:UIColor = UIColor.blackColor()
+    public var subtitleText:String?
+    
+    /*
+     * General Button Attributes
+     */
+    public var buttonFontSize:CGFloat = 13
+    public var buttonCornerRadius:CGFloat = 10
+    public var buttonBorderWidth:CGFloat = 1
+    public var buttonBorderColor:UIColor = UIColor.blackColor()
+    public var buttonWidth:CGFloat = 75
+    public var buttonHeight:CGFloat = 30
+    public var buttonVerticalAligment:ButtonPosition = .Bottom
+    
+    /*
+     * Top Button Attributes
+     */
+    public var topButtonText:String?
+    public var topButtonTextColor:UIColor = UIColor.blackColor()
+    public var topButtonBackgroundColor:UIColor = UIColor.clearColor()
+    
+    
+    /*
+     * Bottom Button Attributes
+     */
+    public var bottomButtonText:String?
+    public var bottomButtonTextColor:UIColor = UIColor.blackColor()
+    public var bottomButtonBackgroundColor:UIColor = UIColor.clearColor()
+    
+    /*
+     * Icon Attribute -> Size is squared
+     */
+    public var imageSquaredSize:CGFloat = 40
+    public var image:UIImage?
+    
+    
+    /*
+     * Dismiss On Tap
+     */
+    public var dismissOnTap:Bool = false
+    
+    
+    
+    // MARK: - INIT Method
+    
+    override public init() {
+        
+        super.init()
+        
+        notificationView = UIView()
+        
+        titleLabel = UILabel()
+        titleLabel?.font = UIFont(name: "HelveticaNeue-Medium", size: titleLabelFontSize)
+        titleLabel?.textColor = titleLabelColor
+        
+        subTitileLabel = UILabel()
+        subTitileLabel?.font = UIFont(name: "HelveticaNeue", size: subTitleLabelFontSize)
+        subTitileLabel?.numberOfLines = 0
+        subTitileLabel?.textColor = subTitleLabelColor
+        
+        imageView = UIImageView()
+        imageView?.image = nil
+        
+        topButton = UIButton(type: .Custom)
+        topButton?.titleLabel?.font = UIFont(name: "HelveticaNeue-Medium", size: buttonFontSize)
+        topButton?.setTitleColor(topButtonTextColor, forState: .Normal)
+        topButton?.adjustsImageWhenHighlighted = true
+        topButton?.backgroundColor = topButtonBackgroundColor
+        
+        topButton?.layer.cornerRadius = buttonCornerRadius
+        topButton?.layer.borderColor = buttonBorderColor.CGColor
+        topButton?.layer.borderWidth = buttonBorderWidth
+        topButton?.layer.masksToBounds = true
+        
+        
+        bottomButton = UIButton(type: .Custom)
+        bottomButton?.titleLabel?.font = UIFont(name: "HelveticaNeue-Medium", size: buttonFontSize)
+        bottomButton?.setTitleColor(bottomButtonTextColor, forState: .Normal)
+        bottomButton?.adjustsImageWhenHighlighted = true
+        bottomButton?.backgroundColor = bottomButtonBackgroundColor
+        
+        bottomButton?.layer.cornerRadius = buttonCornerRadius
+        bottomButton?.layer.borderColor = buttonBorderColor.CGColor
+        bottomButton?.layer.borderWidth = buttonBorderWidth
+        bottomButton?.layer.masksToBounds = true
+        
+        screenSize = UIScreen.mainScreen().bounds.size
+        
+    }
+    
+    // MARK: - PresentView Method
+    
+    public func presentInView(view:UIView, withGravityAnimation animation:Bool) {
+        
+        if isBeingShown == false {
+            
+            imageView?.image = image
+            titleLabel?.text = titleText
+            subTitileLabel?.text = subtitleText
+            topButton?.setTitle(topButtonText, forState: .Normal)
+            bottomButton?.setTitle(bottomButtonText, forState: .Normal)
+            
+            
+            // Calculation
+            let textWidth = Int( (screenSize?.width)! - ( 4 * paddingBetweenElements) - imageSquaredSize - buttonWidth)
+            let titleAttr = [NSFontAttributeName:UIFont(name: "HelveticaNeue-Medium", size: titleLabelFontSize)!]
+            let titleHeight = NSString(string: titleLabel!.text!).boundingRectWithSize(CGSize(width: textWidth,height: 999), options: .UsesLineFragmentOrigin, attributes: titleAttr, context: nil).size.height
+            
+            let subtitleAttr = [NSFontAttributeName:UIFont(name: "HelveticaNeue", size: subTitleLabelFontSize)!]
+            let subtitleHeight = NSString(string: subTitileLabel!.text!).boundingRectWithSize(CGSize(width: textWidth,height: 999), options: .UsesLineFragmentOrigin, attributes: subtitleAttr, context: nil).size.height
+            
+            var notificationHeight = (20 + paddingBetweenElements + titleHeight + (paddingBetweenElements / 2) + subtitleHeight + paddingBetweenElements)
+            
+            if notificationHeight < 100 {
+                notificationHeight = 100
+            }
+            
+            
+            // Notification View
+            notificationView!.frame = CGRect(x: 0, y: -notificationHeight, width: screenSize!.width, height: notificationHeight)
+            
+            //notificationView!.backgroundColor = backgroundColor
+            
+            view.addSubview(notificationView!)
+            view.bringSubviewToFront(notificationView!)
+            
+            
+            if NSFoundationVersionNumber > NSFoundationVersionNumber_iOS_7_1 && backgroundColor == nil {
+                let visualEffect = UIBlurEffect(style: .Light)
+                let blurView = UIVisualEffectView(effect: visualEffect)
+                blurView.frame = (notificationView?.bounds)!
+                notificationView?.addSubview(blurView)
+            }else{
+                notificationView?.backgroundColor = backgroundColor
+            }
+            
+            //Image View
+            
+            imageView?.frame = CGRect(x: paddingBetweenElements, y: (notificationHeight/2)-(paddingBetweenElements/2)+(20/2), width: imageSquaredSize, height: imageSquaredSize)
+            
+            if image != nil {
+                notificationView?.addSubview(imageView!)
+            }
+            
+            // Title
+            
+            titleLabel!.frame = CGRect(x: paddingBetweenElements + imageSquaredSize + paddingBetweenElements, y: 20 + paddingBetweenElements, width: CGFloat(textWidth), height: CGFloat(titleHeight))
+            
+            if titleText != nil {
+                notificationView?.addSubview(titleLabel!)
+            }
+            
+            //Sub Title
+            
+            subTitileLabel!.frame = CGRect(x: paddingBetweenElements + imageSquaredSize + paddingBetweenElements, y: titleLabel!.frame.origin.y + titleLabel!.frame.size.height + 3 , width: CGFloat(textWidth), height: CGFloat(subtitleHeight))
+            //subTitileLabel?.numberOfLines = 99
+            if subtitleText != nil {
+                notificationView?.addSubview(subTitileLabel!)
+            }
+            
+            //Top Button
+            
+            if buttonVerticalAligment == .Top {
+                topButton!.frame = CGRect(x: titleLabel!.frame.origin.x + titleLabel!.frame.size.width + paddingBetweenElements, y: 20 + (paddingBetweenElements / 2), width:  buttonWidth, height: buttonHeight)
+            }else{
+                topButton!.frame = CGRect(x: titleLabel!.frame.origin.x + titleLabel!.frame.size.width + paddingBetweenElements, y: notificationView!.frame.size.height - (paddingBetweenElements * 2 ) - ( buttonHeight * 2) , width: buttonWidth, height: buttonHeight)
+            }
+            
+            
+            topButton?.addTarget(self, action: #selector(self.topButtonTapped), forControlEvents: .TouchUpInside)
+            
+            
+            if topButtonText != nil {
+                notificationView?.addSubview(topButton!)
+            }
+            
+            //Bottom Button
+            
+            bottomButton!.frame = CGRect(x: titleLabel!.frame.origin.x + titleLabel!.frame.size.width + paddingBetweenElements, y: topButton!.frame.origin.y + topButton!.frame.size.height + paddingBetweenElements, width: buttonWidth , height: buttonHeight)
+            
+            bottomButton?.addTarget(self, action: #selector(self.bottomButtonTapped), forControlEvents: .TouchUpInside)
+            
+            
+            if bottomButtonText != nil {
+                notificationView?.addSubview(bottomButton!)
+            }
+            
+            
+            if dismissOnTap == true {
+                let tap = UITapGestureRecognizer(target: self, action: #selector(self.dismiss))
+                tap.numberOfTapsRequired = 1
+                notificationView?.addGestureRecognizer(tap)
+            }
+            
+            if animation == true {
+                
+                animator = UIDynamicAnimator(referenceView: view)
+                let gravity = UIGravityBehavior(items: [notificationView!])
+                animator?.addBehavior(gravity)
+                
+                let collision = UICollisionBehavior(items: [notificationView!])
+                collision.translatesReferenceBoundsIntoBoundary = false
+                collision.addBoundaryWithIdentifier("notificationEnd", fromPoint: CGPoint(x: 0,y: notificationHeight), toPoint: CGPoint(x: screenSize!.width, y: notificationHeight))
+                animator?.addBehavior(collision)
+                
+                let elasticBehavior = UIDynamicItemBehavior(items: [notificationView!])
+                elasticBehavior.elasticity = 0.3
+                animator?.addBehavior(elasticBehavior)
+                
+                
+            }else{
+                
+                UIView.animateWithDuration(0.25, delay: 0, options: .CurveEaseOut, animations: {
+                    self.notificationView!.frame = CGRect(x: 0, y: 0, width:  self.screenSize!.width , height: notificationHeight)
+                    }, completion: nil)
+                
+                
+                
+            }
+            
+            isBeingShown = true
+            gravityAnimation = animation
+            
+            
+            
+            
+        } // end if shown
+        
+        
+        
+        
+    } //end present in view
+    
+    
+    
+    
+    // MARK: -  Delegate Methods
+    
+    @objc private func topButtonTapped(){
+        if delegate != nil {
+            self.delegate?.dropdownNotificationTopButtonTapped()
+        }
+    }
+    
+    @objc private func bottomButtonTapped(){
+        if delegate != nil {
+            self.delegate?.dropdownNotificationBottomButtonTapped()
+        }
+    }
+    
+    // MARK: - Dismiss Methods
+    
+    @objc private func dismiss(){
+        self.delegate?.dropdownNotificationDismissedWithTap()
+        self.dismissWithGravityAnimation(gravityAnimation!)
+    }
+    
+    public func dismissWithGravityAnimation (animation:Bool) {
+        
+        if animation == true {
+            
+            let gravity = UIGravityBehavior(items: [notificationView!])
+            gravity.gravityDirection = CGVector(dx: 0, dy: -1.5)
+            animator?.addBehavior(gravity)
+            
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), {
+                self.animator?.removeAllBehaviors()
+                self.removeSubviews()
+                self.notificationView?.removeFromSuperview()
+                self.isBeingShown = false
+            })
+            
+            
+        }else{
+            
+            UIView.animateWithDuration(0.25, delay: 0, options: .CurveEaseOut, animations: {
+                
+                self.notificationView?.frame = CGRect(x: 0, y: self.notificationView!.frame.size.height , width: self.screenSize!.width, height: self.notificationView!.frame.size.height)
+                
+                }, completion: { (finished) in
+                    
+                    self.removeSubviews()
+                    self.notificationView!.removeFromSuperview()
+                    self.isBeingShown = false
+            })
+            
+            
+        }
+        
+    }
+    
+    func removeSubviews(){
+        for subview in notificationView!.subviews {
+            subview.removeFromSuperview()
+        }
+    }
+    
+    
+    
+}
--- Example/EVWDropDownNotification/ViewController.swift
+++ Example/EVWDropDownNotification/ViewController.swift
@@ -7,18 +7,107 @@
 //
 
 import UIKit
+import EVWDropDownNotification
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, EVWDropDownNotificationDelegate {
+
+    
+    let notification = EVWDropDownNotification()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
+    
+    func basicNotification(){
+        notification.delegate = self
+        notification.image = UIImage(named: "notification")
+        notification.bottomButtonText = nil
+        notification.topButtonText = nil
+        notification.dismissOnTap = false
+        notification.buttonVerticalAligment = .Bottom
+        notification.titleText = "Lorem Ipsum"
+        notification.subtitleText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit."
+    }
+    
+    func displayNotification(){
+        notification.presentInView(self.view.superview!, withGravityAnimation: true)
+    }
+    
+    // MARK: - Only Bottom Button
+    
+    @IBAction func onlyBottom(sender: AnyObject) {
+        basicNotification()
+        notification.bottomButtonText = "Bottom"
+        displayNotification()
+    }
+    
+    // MARK: -  Only Top Button
+    
+    @IBAction func onlyTop(sender: AnyObject) {
+        basicNotification()
+        notification.topButtonText = "Top"
+        displayNotification()
+    }
+    
+    // MARK: - Both Buttons
+    
+    @IBAction func bothButtons(sender: AnyObject) {
+        basicNotification()
+        notification.bottomButtonText = "Bottom"
+        notification.topButtonText = "Top"
+        displayNotification()
+    }
+    
+    // MARK: - Dismiss On Tap
+    
+    @IBAction func dismissOnTap(sender: AnyObject) {
+        basicNotification()
+        notification.dismissOnTap = true
+        notification.bottomButtonText = "Bottom"
+        notification.topButtonText = "Top"
+        displayNotification()
+        
+    }
+    
+    // MARK: - Vertical Aligment Top
+    
+    @IBAction func verticalTop(sender: AnyObject) {
+        basicNotification()
+        notification.buttonVerticalAligment = .Top
+        notification.bottomButtonText = "Bottom"
+        notification.topButtonText = "Top"
+        displayNotification()
+    }
+    
+    // MARK: - Notification Delegates
+    
+    func dropdownNotificationBottomButtonTapped() {
+        notification.dismissWithGravityAnimation(true)
+        print("Bottom Button Tapped")
+    }
+    
+    func dropdownNotificationTopButtonTapped() {
+        notification.dismissWithGravityAnimation(true)
+        print("Top Button Tapped")
+    }
+    
+    func dropdownNotificationDismissedWithTap() {
+        
+        /*
+         * Dismissed with Gravity not required here
+         */
+        
+        print("Dismissed On Tap")
+    }
+
+   
 
 }
 
